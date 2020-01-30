aeb3ef340760912ca68ed3dc5dfde7712a9d8aa5
Zhenying Zhu <zz2283@columbia.edu>
2020-01-29 21:53:32 -0800

    Partially add Project Entity.

---
+++ ToDoAndTracker/Models/Project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAndTracker.Models
+{
+    public enum ProjectType
+    {
+        Work,
+        Household,
+        SelfImprove,
+        Health,
+
+    }
+
+    public class Project
+    {
+        [Key]
+        public int ProjectId;
+
+        public string ProjectName;
+
+        public ProjectType ProjectType;
+
+        public DateTime ProjectCreateTime;
+
+        public DateTime ProjectStartTime;
+
+        public DateTime ProjectDeadline;
+    }
+}
