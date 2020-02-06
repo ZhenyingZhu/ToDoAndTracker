f6cd6d8e8c43a788db5c7f9b0b9a3561de1e0b3c
Zhenying Zhu <zz2283@columbia.edu>
2020-02-05 21:55:10 -0800

    Add a model for tag.

---
+++ ToDoAndTracker/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAndTracker.Models
+{
+    public class Tag
+    {
+        public string Value { get; set; }
+
+        public ICollection<Tag> SubTags { get; set; }
+    }
+}
