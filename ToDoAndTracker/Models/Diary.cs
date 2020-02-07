52503cd6aa87036ff19ec91e637f672aed571d3e
Zhenying Zhu <zz2283@columbia.edu>
2020-02-06 23:56:31 -0800

    Add the last two models.

---
+++ ToDoAndTracker/Models/Diary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAndTracker.Models
+{
+    public class Diary
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Content { get; set; }
+    }
+}
---
+++ ToDoAndTracker/Models/Takeaway.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAndTracker.Models
+{
+    public class Takeaway
+    {
+        public int Id { get; set; }
+
+        public DateTime NextReminderDate { get; set; }
+
+        public string Content { get; set; }
+    }
+}
