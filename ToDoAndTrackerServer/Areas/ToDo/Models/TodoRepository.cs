0e672ad9c95f2912a91f62ed2c395907d08ec955
Zhenying Zhu <zz2283@columbia.edu>
2022-10-24 19:00:34 -0700

    Add Repo DI

--- ToDoAndTrackerServer/Areas/ToDo/Models/TodoRepository.cs
+++ ToDoAndTrackerServer/Areas/ToDo/Models/TodoRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace ToDoAndTrackerServer.Areas.ToDo.Models
+﻿using Microsoft.AspNetCore.Identity;
+using ToDoAndTrackerServer.Data;
+
+namespace ToDoAndTrackerServer.Areas.ToDo.Models
 {
     public class TodoRepository
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<ApplicationDbContext> _logger;
+
+        public TodoRepository(ApplicationDbContext context, UserManager<IdentityUser> userManager, ILogger<ApplicationDbContext> logger)
+        {
+            _context = context;
+            _userManager = userManager;
+            _logger = logger;
+        }
     }
 }
