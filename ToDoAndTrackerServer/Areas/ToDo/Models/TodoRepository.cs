f67b83638befd759bf16039eae1626e36807d4bf
Zhenying Zhu <zz2283@columbia.edu>
2022-10-28 17:23:24 -0700

    Add get user method

--- ToDoAndTrackerServer/Areas/ToDo/Models/TodoRepository.cs
+++ ToDoAndTrackerServer/Areas/ToDo/Models/TodoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 using ToDoAndTrackerServer.Data;
 
 namespace ToDoAndTrackerServer.Areas.ToDo.Models
@@ -15,5 +16,24 @@ namespace ToDoAndTrackerServer.Areas.ToDo.Models
             _userManager = userManager;
             _logger = logger;
         }
+
+        private async Task<string> GetUserIdAsync()
+        {
+            // TODO: Migrate to the new way of dotnet core
+            if (ClaimsPrincipal.Current != null)
+            {
+                IdentityUser user = await _userManager.GetUserAsync(ClaimsPrincipal.Current);
+
+                _logger.LogInformation($"Logging in user: {user.Id}");
+
+                return user.Id;
+            }
+            else
+            {
+                _logger.LogInformation($"ClainsPricipal is null");
+                return "Not logged in";
+            }
+        }
+
     }
 }
