7f1439df9baa3df574b13296cb3384392f4268fd
Zhenying Zhu <zz2283@columbia.edu>
2022-11-03 23:32:00 -0700

    Add Task

--- ToDoAndTrackerServer/Areas/ToDo/Models/Project.cs
+++ ToDoAndTrackerServer/Areas/ToDo/Models/Project.cs
@@ -1,11 +1,18 @@
-﻿namespace ToDoAndTrackerServer.Areas.ToDo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAndTrackerServer.Areas.ToDo.Models
 {
     public class Project
     {
         public int Id { get; set; }
 
+        [Required]
+
         public string OwnerId { get; set; } = null!;
 
+        [Required]
         public string Name { get; set; } = null!;
+
+        public ICollection<Task> TodoItems { get; set; } = null!;
     }
 }
---
+++ ToDoAndTrackerServer/Areas/ToDo/Models/Task.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoAndTrackerServer.Areas.ToDo.Models
+{
+    public enum TaskState
+    {
+        InProgress,
+        Done,
+        Backlog,
+        Abandoned,
+    }
+
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string OwnerId { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public int ProjectId { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public Project Project { get; set; } = null!;
+
+        public TaskState State { get; set; }
+
+    }
+}
