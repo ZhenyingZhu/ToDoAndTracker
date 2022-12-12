583095081ceec6ffca4b238cc1da100f07cabb60
Zhenying Zhu <zz2283@columbia.edu>
2022-12-11 23:07:12 -0800

    Drop todoDTO table

---
+++ ToDoAndTrackerServer/Data/Migrations/20221212070615_RemoveTodoDTOTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoAndTrackerServer.Data.Migrations
+{
+    public partial class RemoveTodoDTOTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoTaskDTO");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoTaskDTO",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectId = table.Column<int>(type: "int", nullable: false),
+                    ProjectName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoTaskDTO", x => x.Id);
+                });
+        }
+    }
+}
